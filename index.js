4fd544959636286b62495b34ab68065ebc183311
Mikkel Ridley <mikkel250@gmail.com>
2017-11-25 23:18:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,10 @@ function johnLennonFacts(factsArray) {
 
 function iLoveTheBeatles(num) {
   var newArray = [];
-  var i = 0;
   do {
-  newArray[i] = "I love the Beatles!";
+  newArray.push("I love the Beatles!");
   }
   while (newArray.length < 15);
-  i++;
+  num++;
   
 }
