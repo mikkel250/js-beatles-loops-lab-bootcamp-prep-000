f4b3a43f4634dfa98f733f8aa738197203bc7690
Mikkel Ridley <mikkel250@gmail.com>
2017-11-25 22:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicianArray, instrumentArray) {
   var beatlesArray = [];
   for (var i = 0; i <= musicianArray.length; i++) {
-    
+    beatlesArray[i] = [musicianArray + instrumentArray]
   }
 }
